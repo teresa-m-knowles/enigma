a82102610932096241cca4d8606284aa88b06129
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-04 10:22:13 -0700

    tests that key can randomly generate five numbers

--- test/key_test.rb
+++ test/key_test.rb
@@ -9,6 +9,17 @@ class KeyTest < Minitest :: Test
     key = Key.new
 
     assert_instance_of Key, key
+  end
+
+  def test_it_can_randomly_generate_five_numbers
+
+    key = Key.new
+
+    key.abcd.all? do |letter|
+      assert_instance_of Integer, letter
+    end
+
+    assert_equal 5, key.abcd.count
 
   end
 
