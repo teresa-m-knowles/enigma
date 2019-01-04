85bba31796c99690c78d2d4e480d1969479622c9
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-04 10:23:27 -0700

    creates abcd method to access the randomly genrated integers

--- lib/key.rb
+++ lib/key.rb
@@ -1,2 +1,18 @@
 class Key
-end 
+
+  attr_reader :a, b, c, d
+  def initialize
+    @a = Random.new.rand(9)
+    @b = Random.new.rand(9)
+    @c = Random.new.rand(9)
+    @d = Random.new.rand(9)
+  end
+
+  def abcd
+    return a,b,c,d
+  end
+
+
+
+
+end
