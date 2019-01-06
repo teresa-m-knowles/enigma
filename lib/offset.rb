18bda7a10cb3f54e569cb5b6e4b95ec73fe6518a
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-05 17:33:53 -0700

    initial method to get date of tranmission

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,11 +1,35 @@
 require 'date'
+require 'pry'
 
 class Offset
 
-  attr_rea
+  attr_accessor :date
 
-  def initialize(date = TODAY)
-    @date = date
+  def initialize(date = nil)
+    if date
+      @date = date.to_s
+      divide_string
+    else
+      @date = Date.today.to_s
+      format_date_to_DDMMYY
+    end
+    binding.pry
+    format_date_to_DDMMYY
   end
+
+  def format_date_to_DDMMYY
+    divided = @date.split('-')
+    day = divided[2]
+    month = divided[1]
+    year = divided[0][2..-1]
+    @date = day + month + year
+  end
+
+  def divide_string
+    day = @date[0..1]
+    month = @date[2..3]
+    year = @date[4..5]
+  end
+
 
 end
