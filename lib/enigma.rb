5028be6e3928f1679550264f390d5f1e307f3171
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-05 23:52:37 -0700

    adds rotate_back method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -30,7 +30,7 @@ class Enigma
     message.downcase.split('').each_slice(4).to_a
   end
 
-  def rotate(given_message, shift)
+  def rotate_forward(given_message, shift)
     split_message_in_fours(given_message).map do |set_of_four|
       encoded = ""
       set_of_four.map.with_index do |char, i|
@@ -58,7 +58,39 @@ class Enigma
     date = create_date(given_date)
     key = create_key(given_key)
     shift = Shift.new(key,date)
-    {:encryption => rotate(given_message,shift), :key=> key.numbers, :date => date.date  }
+    {:encryption => rotate_forward(given_message,shift), :key=> key.numbers, :date => date.date  }
+  end
+
+  def decrypt(given_message, key, date = nil)
+    key = create_key(key)
+    date = create_date(date)
+    shift = Shift.new(key,date)
+    {:decryption => rotate_back(given_message,shift), :key=> key.numbers, :date => date.date  }
+
+  end
+
+  def rotate_back(given_message, shift)
+    split_message_in_fours(given_message).map do |set_of_four|
+      encoded = ""
+      set_of_four.map.with_index do |char, i|
+        index_in_alphabet = @character_set.index(char)
+        if !@character_set.include?(char)
+          encoded += char
+        else
+          if i == 0
+            encoded += char = @character_set.rotate(-shift.a)[index_in_alphabet]
+          elsif i == 1
+            encoded += char = @character_set.rotate(-shift.b)[index_in_alphabet]
+          elsif i == 2
+            encoded += char = @character_set.rotate(-shift.c)[index_in_alphabet]
+          elsif i == 3
+            encoded += char = @character_set.rotate(-shift.d)[index_in_alphabet]
+          end
+        end
+      end
+      encoded
+    end.join
+
   end
 
 
