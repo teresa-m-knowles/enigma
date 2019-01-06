3610e161dda324ffa251dd3dc33e10bfa0ea578b
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-05 20:54:48 -0700

    creates shift class

---
+++ lib/shift.rb
@@ -0,0 +1,21 @@
+require './lib/key'
+require './lib/offset'
+
+class Shift
+
+  attr_reader :key, :offset
+  attr_accessor :a, :b, :c, :d
+
+  def initialize(key, offset)
+    @key = key
+    @offset = offset
+    find_each_shift
+  end
+
+  def find_each_shift
+    @a = key.a + offset.a
+    @b = key.b + offset.b
+    @c = key.c + offset.c
+    @d = key.d + offset.d
+  end
+end
