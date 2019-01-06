692d7d5996e0fb1efba38055ccf93042df83c2b2
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-05 21:29:52 -0700

    creates encrypt test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -18,4 +18,16 @@ class EnigmaTest < Minitest::Test
 
     assert_equal expected, enigma.character_set
   end
+
+  def test_it_can_encrypt
+
+    enigma = Enigma.new
+    expected =  {
+                  encryption: "keder ohulw",
+                  key: "02715",
+                  date: "040895"
+   }
+
+   assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
+  end
 end
