6c713a1d0a9be0a78a0ca5f5c06531e5584574f7
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-05 21:11:23 -0700

    creates test for enigma method that creates character set

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,4 +10,12 @@ class EnigmaTest < Minitest::Test
 
     assert_instance_of Enigma, enigma
   end
+
+  def test_it_contains_a_character_set_of_the_alphabet_and_a_space
+
+    enigma = Enigma.new
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal expected, enigma.character_set
+  end
 end
