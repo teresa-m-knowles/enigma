1e50b78ca5772daee640ba420ba7ba4b2fc8ea21
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-05 17:33:32 -0700

    adds date of transmission to offset tests

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -12,7 +12,18 @@ class OffsetTest < Minitest::Test
     assert_instance_of Offset, offset
   end
 
-  def test_it_has_a_date
+  def test_it_has_a_date_of_transmission
+
+    offset = Offset.new
+    assert offset.date
+    assert_equal 6, offset.date.length
+  end
+  def test_it_can_be_assigned_a_date
+    binding.pry
+    offset = Offset.new('010109')
+
+    assert_equal '010109', offset.date
+
 
   end
 end
