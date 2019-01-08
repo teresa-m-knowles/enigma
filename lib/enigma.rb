0871c94d34e8744493b977fc95a697671f4dca0f
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-08 12:49:21 -0700

    takes first crack at crack

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,7 @@
 require './lib/key'
 require './lib/shift'
 require './lib/offset'
+require 'pry'
 
 class Enigma
 
@@ -59,14 +60,16 @@ class Enigma
     key = create_key(given_key)
     shift = Shift.new(key,date)
     {:encryption => rotate_forward(given_message,shift), :key=> key.numbers, :date => date.date  }
+    binding.pry
   end
 
   def decrypt(given_message, key, date = nil)
     key = create_key(key)
     date = create_date(date)
     shift = Shift.new(key,date)
-    {:decryption => rotate_back(given_message,shift), :key=> key.numbers, :date => date.date  }
-
+              {:decryption => rotate_back(given_message,shift),
+                :key=> key.numbers,
+                :date => date.date  }
   end
 
   def rotate_back(given_message, shift)
@@ -95,8 +98,44 @@ class Enigma
 
   def crack(encrypted_message,date = nil)
     date = create_date(date)
-    ending = " end"
+    array_of_chars = split_message_in_fours(encrypted_message)
+    last_four = array_of_chars.flatten[-4..-1]
+    actual_a_index = @character_set.index(" ")
+    actual_b_index = @character_set.index("e")
+    actual_c_index = @character_set.index("n")
+    actual_d_index = @character_set.index("d")
+
+    index_a = @character_set.index(last_four[0])
+    index_b = @character_set.index(last_four[1])
+    index_c = @character_set.index(last_four[2])
+    index_d = @character_set.index(last_four[3])
+
+    shift = Shift.new
+
+    shift.a = index_a + 1
+    shift.b = index_b - actual_b_index
+    shift.c = index_c - actual_c_index
+    shift.d = index_d - actual_d_index
+
+    key_a = shift.a - date.a
+    key_b = shift.b - date.b
+    key_c = shift.c - date.c
+    key_d = shift.d - date.d
+    binding.pry
+
+
+
+
+    binding.pry
+    #
+    # last_four.map.with_index do |char,i|
+    #   index_in_alphabet = @character_set.index(char)
+    #   @character_set.index(ends[0])
+    #   binding.pry
+    #   shift.a
+    # end
 
   end
+
 
 end
